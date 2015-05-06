873a4d82369ec06fae6337994316d28bdbf7fdb9
santoshjinna <santhoshrdd18@gmail.com>
2015-05-06 00:24:15 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,9 +4,7 @@ require_relative 'models'
 
 set :bind, '0.0.0.0'
 set :port, 8080
-get '/' do
-  "Hello World"
-end  
+ 
 def configure_database
   if ENV['RACK_ENV']=="production"
     while !self.connect_to_database_prod
